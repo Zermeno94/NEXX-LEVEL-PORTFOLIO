3429e56057faadcac653ddc27212b06393bc360e
MIRANDA <miranda.zermeno4@gmail.com>
2021-12-30 23:08:46 -0600

    added images folder in public and started added images for portfolio

---
+++ my-app/src/compnents/resumeInfo.js
@@ -0,0 +1,91 @@
+const resumeInfo= {
+    "imagebaseurl":"https://zermeno94.github.io/",
+    "name": "Miranda Zermeno",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "Enter content here.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/miranda-zermeno/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Zermeno94",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Dallas,Tx",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"University of Texas  at Arlington",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"",
+        "specialization":"El Centro Community College",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ]
+    
+  }
+  
+  export default resumeInfo
