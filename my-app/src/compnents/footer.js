8935688c56652ceb623fd29469635361742a351c
MIRANDA <miranda.zermeno4@gmail.com>
2021-12-30 22:48:43 -0600

    added footer.js with code

---
+++ my-app/src/compnents/footer.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+export default class Footer extends Component {
+  render() {
+    const resumeData = this.props.resumeInfo;
+    return (
+      <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">
+            {
+              resumeInfo.socialLinks && resumeInfo.socialLinks.map((item)=>{
+                return(
+                  <li>
+                    <a href={item.url}>
+                    <i className={item.className} />
+                    </a>
+                  </li>
+                )
+              })
+            }
+          </ul>
+          
+        </div>
+        <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open" /></a></div>
+      </div>
+    </footer>
+    );
+  }
+}
