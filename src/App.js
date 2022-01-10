7a1a8730c3a531ebf13a6ec7ab9ce1f55cee8365
MIRANDA <miranda.zermeno4@gmail.com>
2022-01-09 19:09:14 -0600

    refactoring

--- src/App.js
+++ src/App.js
@@ -15,9 +15,10 @@ import Footer from "./components/footer";
 // import Skills from "./components/Skills";
 // import Testimonials from "./components/Testimonials";
 
-export default function App() {
+const App = ()  =>{
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
+     
       <Header />
       <About />
       <Portfolio /> 
@@ -29,6 +30,7 @@ export default function App() {
   );
 }
 
+export default App
 
 
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,6 @@
-import React, { Component } from 'react';
-export default class Header extends Component {
-  render() {
-    const resumeInfo = this.props.resumeInfo;
-    return (
+import React from "react";
+const Header = () => {
+ return (
       <React.Fragment>
       
       <header id="home">
@@ -22,9 +20,9 @@ export default class Header extends Component {
             </ul>
          </nav>
 
-         <div className="row banner">
+         {/* {/* <div className="row banner">
             <div className="banner-text">
-               <h1 className="responsive-headline">I am {resumeInfo.name}.</h1>
+               <h1 className="responsive-headline">Miranda Zermeno</h1>
                <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeInfo.role}.{resumeInfo.roleDescription}
                </h3>
                <hr/>
@@ -45,10 +43,70 @@ export default class Header extends Component {
 
          <p className="scrolldown">
             <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
-         </p>
-
-      </header>
+         </p> */}
+                
+                </header> 
       </React.Fragment>
     );
-  }
 }
+
+export  default  Header
+
+// Per BCS Assist:
+// const Header = () => { <React.Fragment></React.Fragment>} export default 
+
+
+// import React, { Component } from 'react';
+// export default class Header extends Component {
+//   render() {
+//     const resumeInfo = this.props.resumeInfo;
+//     return (
+//       <React.Fragment>
+      
+//       <header id="home">
+//          <nav id="nav-wrap">
+//             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+//           <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+//             <ul id="nav" className="nav">
+//                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
+
+//                <li><a className="smoothscroll" href="#about">About</a></li>
+
+//              <li><a className="smoothscroll" href="#resume">Resume</a></li>
+
+//                <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+
+//                <li><a className="smoothscroll" href="#contact">Contact</a></li>
+//             </ul>
+//          </nav>
+
+//          <div className="row banner">
+//             <div className="banner-text">
+//                <h1 className="responsive-headline">I am {resumeInfo.name}.</h1>
+//                <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeInfo.role}.{resumeInfo.roleDescription}
+//                </h3>
+//                <hr/>
+//                <ul className="social">
+//                   {
+//                     resumeInfo.socialLinks && resumeInfo.socialLinks.map(item =>{
+//                       return(
+//                               <li key={item.name}>
+//                                 <a href={item.url} target="_blank"><i className={item.className}></i></a>
+//                               </li>
+//                             )
+//                           }
+//                     )
+//                   }
+//                </ul>
+//             </div>
+//          </div>
+
+//          <p className="scrolldown">
+//             <a className="smoothscroll" href="#about"><i className="icon-down-circle"></i></a>
+//          </p>
+
+//       </header>
+//       </React.Fragment>
+//     );
+//   }
+// }
