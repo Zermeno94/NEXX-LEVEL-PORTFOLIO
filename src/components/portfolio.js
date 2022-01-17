16ec603b9802147df9a09891d681f9b21fdcd070
MIRANDA <miranda.zermeno4@gmail.com>
2022-01-17 16:02:21 -0600

    adjusting links to projects

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  Container,Carousel ,Image} from 'react-bootstrap';
+import { Container, Carousel, Image, NavLink } from 'react-bootstrap';
 
 import '../style/style.css';
 import Sectiontitle from './Sectiontitle';
@@ -7,33 +7,36 @@ import Sectiontitle from './Sectiontitle';
 const Portfolio = () => {
   return (
     <>
-    <Container fluid='true' className='container'>
-    <Sectiontitle title = 'Projects' />
-    <Carousel>
-  <Carousel.Item>
-  <Image src={require('../images/GifCast.gif')} />
-    <Carousel.Caption>
-      <h3>GifCast</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-  <Image src={require('../images/Work Day Scheduler.png')} />
+      <Container fluid='true' className='container'>
+        <Sectiontitle title='Projects' />
+        <Carousel>
+          <Carousel.Item>
+           
+             <a href="https://github.com/MrTofuuu/GifCast" target="_blank" rel="noopener noreferrer"><Image src={require('../images/GifCast.gif')} 
+             /></a>
 
-    <Carousel.Caption>
-      <h3>Work Day Scheduler</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-  <Image src={require('../images/E-Book Shop.png')} />
+            <Carousel.Caption>
+              <h3>GifCast</h3>
+              <p>This product allows you to search the weather forecast and it will display your current day + 4 day forecast for weather and the gif related according to the weather rating .</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={require('../images/Work Day Scheduler.png')} />
 
-    <Carousel.Caption>
-      <h3>E-Book Shop</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-     </Carousel.Caption>
-     </Carousel.Item>
-     </Carousel>
+            <Carousel.Caption>
+              <h3>Work Day Scheduler</h3>
+              <p>Create a simple calendar application that allows a user to save events for each hour of the day.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image src={require('../images/E-Book Shop.png')} href='#https://github.com/mjs-esfsc/SMU-Project-2' />
+
+            <Carousel.Caption>
+              <h3>E-Book Shop</h3>
+              <p>Off the Shelf is a web application where users can create an account and browse through numerous book genres. Users can selected a book to purchase or save it in their e-bookshelf history.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </Container>
     </>
   );
